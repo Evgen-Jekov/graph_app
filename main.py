93d65ad0ecb3d2a3ada2549c5260a13fb15aaa50
Evgen-Jekov <zekovevgen992@gmail.com>
2025-05-06 01:46:06 +0000

    start desctop

---
+++ main.py
@@ -0,0 +1,9 @@
+from app.gui.main_window import MainWindow
+from PyQt6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = MainWindow()
+
+    app.exec()
