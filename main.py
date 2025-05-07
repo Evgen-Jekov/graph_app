8000a4d0aae0087e1399d2ba7cc8938478e41229
Evgen-Jekov <zekovevgen992@gmail.com>
2025-05-07 02:55:56 +0000

    create database add

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from app.gui.main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
+
+from app.gui.main_window import MainWindow
+from app.db.get_db import base, engine
+from app.db.model import Graph
+
 
 if __name__ == "__main__":
     app = QApplication([])
+
+    base.metadata.create_all(bind=engine)
 
     window = MainWindow()
 
